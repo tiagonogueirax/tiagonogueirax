039098bd1097431ad0734757054c8959a97856dd
Tiago Nogueira <tiagonogueirax@gmail.com>
2021-11-24 01:05:13 -0300

    reset

--- autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-function __autoload($class) {
-	require_once 'include/model/'.$class.'.class.php';
-}
--- config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-const URL_HOME = 'http://localhost/bibliotech';
-const SITE_TITLE = 'BiblioTech';
